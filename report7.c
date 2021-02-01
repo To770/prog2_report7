e9ae89a3e0c6534227e2bb617dbedfd07f96b27c
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-02-02 03:24:17 +0900

    for report7

---
+++ report7.c
@@ -0,0 +1,36 @@
+/*点数が高い順に並び替えるための関数*/
+
+#include <stdio.h>
+
+void sort(int array[], int size);/*関数プロトタイプ宣言*/
+
+void sort(int array[], int size){
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            if(array[i] < array[j]){
+                int num = array[i];
+                array[i] = array[j];
+                array[j] = num;
+            }
+        }
+    }
+    for(int i = 0; i < size; i++){
+        printf("%d ", array[i]);
+    }
+}
+
+int main(){
+    /*定数として配列のサイズを宣言*/
+    const int SIZE = 6; 
+    int scores[SIZE] = {100, 60, 70, 100, 90, 80};
+
+    printf("scores = ");
+    for(int i = 0; i < SIZE; i++){
+        printf("%d ", scores[i]);
+    }
+    printf("\nresults = ");
+    sort(scores, SIZE);
+    printf("\n");
+
+    return 0;
+}
